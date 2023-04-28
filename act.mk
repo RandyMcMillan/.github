3b230b7404d6615a24aa480eddaf8cb46e883c34
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-04-28 19:16:34 -0400

    make: act-automate - and some formatting

--- act.mk
+++ act.mk
@@ -1,2 +1,13 @@
-act-autogen-alpine:
-	@export $(cat ~/GH_TOKEN.txt) && act -vbr -W .github/workflows/autogen_alpine.yml 
+default:act-help
+act-help:### 	act-help
+	@sed -n 's/^###//p' ${MAKEFILE_LIST} | sed -e 's/://'
+	@echo ""
+	@echo "act-autogen-alpine           act-autogen-alpine"
+	@echo "act-autogen-alpine-reuse     act-autogen-alpine-reuse"
+
+act-automate:###
+	@export $(cat ~/GH_TOKEN.txt) && act -vb -W .github/workflows/automate.yml
+act-autogen-alpine:###
+	@export $(cat ~/GH_TOKEN.txt) && act -vb -W .github/workflows/autogen_alpine.yml
+act-autogen-alpine-reuse:###
+	@export $(cat ~/GH_TOKEN.txt) && act -vbr -W .github/workflows/autogen_alpine.yml
