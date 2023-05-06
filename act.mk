5a20832c60878aef23602f0d76177a54fed10dd4
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-06 19:08:53 -0400

    make: act - act.mk $(MAKE) docker-start

--- act.mk
+++ act.mk
@@ -1,2 +1,3 @@
 act-autogen-alpine:## 	run act in .github
+	$(MAKE) docker-start
 	@export $(cat ~/GH_TOKEN.txt) && act -v $(BIND) $(REUSE)  --secret $(GH_ACT_TOKEN)  --userns $(PROJECT_NAME)  -W .github/workflows/alpine.yml
