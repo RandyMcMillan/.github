c4f8ebc06f150c7ca5bcf80dee2aec5f2b1e87b9
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-12-06 11:19:37 -0500

    autogen.sh:update

--- act.mk
+++ act.mk
@@ -1,6 +1,6 @@
 alpine:## 	run act in .github
 	$(MAKE) docker-start
-	@export GH_ACT_TOKEN=$(cat ~/GH_TOKEN.txt) && act -v $(BIND) $(REUSE)  --secret $(GH_ACT_TOKEN)  --userns $(PROJECT_NAME)  -W .github/workflows/$@.yml
+	@export GITHUB_TOKEN=$(cat ~/GITHUB_TOKEN.txt) && act -v $(BIND) $(REUSE)  --userns $(PROJECT_NAME)  -W .github/workflows/$@.yml
 automate:## 	run act in .github
 	$(MAKE) docker-start
-	@export GH_ACT_TOKEN=$(cat ~/GH_TOKEN.txt) && act -v $(BIND) $(REUSE)  --secret $(GH_ACT_TOKEN)  --userns $(PROJECT_NAME)  -W .github/workflows/$@.yml
+	@export GH_ACT_TOKEN=$(cat ~/GH_TOKEN.txt) && act -v $(BIND) $(REUSE)  --userns $(PROJECT_NAME)  -W .github/workflows/$@.yml
